db746622b7be07e1e5507c17ea0765de8cc4e59f
mmm2306 <mmm2306@gmail.com>
2019-03-26 21:42:03 +0200

    fixed relative paths issue

--- src/app/guest-components/guest-layout/guest-layout.component.ts
+++ src/app/guest-components/guest-layout/guest-layout.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/guards/auth.service';
+import { AuthService } from '../../guards/auth.service';
 
 @Component({
   selector: 'app-guest-layout',
